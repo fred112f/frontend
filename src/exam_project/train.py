24b7bc5d0db4a8806a99cb91710ee8e147035165
krusand <110490348+krusand@users.noreply.github.com>
2026-01-07 23:53:46 +0100

    chore: initial commit

---
+++ src/exam_project/train.py
@@ -0,0 +1,10 @@
+from exam_project.model import Model
+from exam_project.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
