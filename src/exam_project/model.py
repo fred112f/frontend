fa752c965ac1683d1761468ffb032950d7a05604
SebWae <s254120@dtu.dk>
2026-01-09 14:18:08 +0100

    dimension matching for custom CNN

--- src/exam_project/model.py
+++ src/exam_project/model.py
@@ -5,10 +5,14 @@ import torch.nn.functional as F
 
 class CustomCNN(nn.Module):
     """Our custom CNN to classify facial expressions."""
-    def __init__(self):
+    def __init__(self, img_size: int, output_dim: int):
         super(CustomCNN, self).__init__()
+
+        self.img_size = img_size
+        self.output_dim = output_dim
+
         # Convolutional layers
-        self.conv1 = nn.Conv2d(3, 48, kernel_size=3, padding=1)
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)
         self.bn1 = nn.BatchNorm2d(32)
 
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
@@ -22,24 +26,38 @@ class CustomCNN(nn.Module):
 
         self.pool = nn.MaxPool2d(2, 2)
         self.dropout = nn.Dropout(0.6)   # increased dropout
-        self.gap = nn.AdaptiveAvgPool2d(1)  # Global Average Pooling
 
         # Fully connected layers
-        self.fc1 = nn.Linear(256, 256)
-        self.fc2 = nn.Linear(256, 7)
+        self.fc1 = nn.Linear(3*3*256, 256)   # multiplying 
+        self.fc2 = nn.Linear(256, output_dim)
+
+        # Loss function
+        self.loss_fn = nn.NLLLoss()
 
     def forward(self, x):
         x = self.pool(F.relu(self.bn1(self.conv1(x))))
         x = self.pool(F.relu(self.bn2(self.conv2(x))))
         x = self.pool(F.relu(self.bn3(self.conv3(x))))
-        x = self.pool(F.relu(self.bn4(self.conv4(x))))
-        x = self.gap(x)                # GAP reduces to (B, 256, 1, 1)
-        x = torch.flatten(x, 1)        # flatten to (B, 256)
+        x = self.pool(F.relu(self.bn4(self.conv4(x))))              
+        x = torch.flatten(x, 1)        
         x = self.dropout(F.relu(self.fc1(x)))
         x = self.fc2(x)
+        x = F.log_softmax(x, dim=1)
         return x
 
+    def training_step(self, batch):
+        img, target = batch
+        y_pred = self(img)
+        return self.loss_fn(y_pred, target)
+    
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=1e-3)
+
 if __name__ == "__main__":
-    model = CustomCNN()
-    x = torch.rand(1)
-    print(f"Output shape of model: {model(x).shape}")
+    model = CustomCNN(img_size=48, output_dim=7)
+    # Create a random input tensor with shape (batch=1, channels=1, H=img_size, W=img_size)
+    x = torch.rand(2, 1, model.img_size, model.img_size)
+    output = model(x)
+    print(f"Input: {x}")
+    print(f"Output shape of model: {output.shape}")
+    print(f"Output: {torch.exp(output)}")
