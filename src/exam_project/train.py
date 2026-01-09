5d57a98529325fc0c54f8d4b848530e12ec64f5c
krusand <110490348+krusand@users.noreply.github.com>
2026-01-09 13:47:08 +0100

    feat: added loading dataset

--- src/exam_project/train.py
+++ src/exam_project/train.py
@@ -1,11 +1,46 @@
 from exam_project.model import Model
-from exam_project.data import load_data
 from pytorch_lightning import LightningModule, Trainer
 from torch import nn, optim
 import torch
 import transformers
 import torchvision
 import PIL
+
+from PIL import Image
+from torchvision import transforms
+from torchvision.datasets import DatasetFolder
+import os
+
+# Function to open images
+def pil_loader(path):
+    with open(path, 'rb') as f:
+        img = Image.open(f)
+        return img.convert('L')# grayscale==1 channel
+# Transform: convert to tensor and optionally normalize
+
+def get_transform():
+    transform = transforms.Compose([
+        transforms.ToTensor(), # converts PIL image to tensor with values [0,1]
+    ])
+    return transform
+def get_dataset(root, transform):
+    dataset = DatasetFolder(
+        root=root, # replace with your folder path
+        loader=pil_loader,
+        extensions=['jpg'],
+        transform=transform
+    )
+    return dataset
+
+
+def load_data():
+    root = 'data/raw/datasets/msambare/fer2013/versions/1/'
+    transform = get_transform()
+    train_dataset = get_dataset(os.path.join(root,'train'), transform)
+    test_dataset = get_dataset(os.path.join(root,'test'), transform)
+    return train_dataset, test_dataset, test_dataset
+
+    
 
 def get_trainer(model, trainer_args):
     """
@@ -28,11 +63,12 @@ def train():
     Trains the model
     """
     trainer_args = dict()
-    train, val, test = load_data("data/raw")
+    train, val, test = load_data()
     model = Model()
     trainer = get_trainer(model, trainer_args=trainer_args)
     trainer.fit(model=model)
     torch.save(model.state_dict(), "models/checkpoint.pth")
+
 
 
 def load():
