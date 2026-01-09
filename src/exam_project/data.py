f9abae36cba451632d2e55e918ef789f8f6bfc13
Sam M. Ward <smward1@hotmail.com>
2026-01-09 11:28:24 +0100

    Changed datapath to data/raw/

--- src/exam_project/data.py
+++ src/exam_project/data.py
@@ -3,7 +3,7 @@ import os
 
 if __name__ == "__main__":
     #Set KAGGLEHUB_CACHE environment variable
-    os.environ["KAGGLEHUB_CACHE"] = "data/"
+    os.environ["KAGGLEHUB_CACHE"] = "data/raw/"
 
     #Download latest version
     path = kagglehub.dataset_download("msambare/fer2013")
