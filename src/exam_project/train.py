1bfa32f16302b6d12d3fe224f4c814906bcc40d1
krusand <110490348+krusand@users.noreply.github.com>
2026-01-16 09:58:15 +0100

    changed config location + more

--- src/exam_project/train.py
+++ src/exam_project/train.py
@@ -16,9 +16,7 @@ api_key = os.getenv("WANDB_API_KEY")
 entity = os.getenv("WANDB_ENTITY")
 project = os.getenv("WANDB_PROJECT")
 
-DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
-
-@hydra.main(config_path="../../configs", config_name="train", version_base=None)
+@hydra.main(config_path="configs", config_name="train", version_base=None)
 def train(cfg):
     """
     Trains the model
@@ -26,30 +24,27 @@ def train(cfg):
     params: 
         cfg: config.yaml using Hydra
     """
-    print(cfg)
 
-    exit()
-    wandb_logger = WandbLogger(log_model="all", project=project)
     checkpoint_callback = ModelCheckpoint(
         monitor='validation_loss',
         dirpath='models/',
         filename='emotion-model-{epoch:02d}-{validation_loss:.2f}'
     )
 
-    trainer_args = {"max_epochs": max_epochs
+    trainer_args = {"max_epochs": cfg.trainer.max_epochs
                     ,'limit_train_batches': 0.05
-                    , 'accelerator': DEVICE
-                    , 'logger': wandb_logger
+                    , 'accelerator': cfg.trainer.accelerator
+                    , 'logger': instantiate(cfg.logger.wandb)
                     , 'log_every_n_steps': 5
                     , "callbacks": [checkpoint_callback]}
     
     train, val, test = load_data(processed_dir='data/processed/')
-    train = torch.utils.data.DataLoader(train, persistent_workers=True, num_workers=9, batch_size=batch_size)
-    val = torch.utils.data.DataLoader(val, persistent_workers=True, num_workers=9, batch_size=batch_size)
-    test = torch.utils.data.DataLoader(test, persistent_workers=True, num_workers=9, batch_size=batch_size)
+    train = torch.utils.data.DataLoader(train, persistent_workers=True, num_workers=9, batch_size=cfg.trainer.batch_size)
+    val = torch.utils.data.DataLoader(val, persistent_workers=True, num_workers=9, batch_size=cfg.trainer.batch_size)
+    test = torch.utils.data.DataLoader(test, persistent_workers=True, num_workers=9, batch_size=trainer.batch_size)
     
-    model = BaseCNN(lr=lr)
-    trainer = Trainer(**trainer_args)
+    model = instantiate(cfg.models)
+    trainer = instantiate(cfg.trainer)
     trainer.fit(model=model, train_dataloaders=train, val_dataloaders=val)
     print(checkpoint_callback.best_model_path)
 
