f83bbaeea7eb5f1d041253c94fc830c896834696
krusand <110490348+krusand@users.noreply.github.com>
2026-01-21 12:57:47 +0100

    feat: stage model test

---
+++ tests/performancetests/test_model.py
@@ -0,0 +1,36 @@
+import wandb
+import os
+import time
+from exam_project.model import BaseANN, BaseCNN, ViTClassifier
+import torch
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def load_model(artifact):
+
+    api = wandb.Api(
+        api_key=os.getenv("WANDB_API_KEY"),
+        overrides={"entity": os.getenv("WANDB_ENTITY_ORG"), "project": os.getenv("WANDB_PROJECT")},
+    )
+
+    artifact = api.artifact(f"{os.getenv("WANDB_ENTITY_ORG")}/{os.getenv("WANDB_REGISTRY")}/{os.getenv("MODEL_NAME")}",type="model")
+    artifact.download(root="./artifacts")
+    file_name = artifact.files()[0].name
+    return BaseANN.load_from_checkpoint(f"./artifacts/{file_name}")
+
+def test_model_speed():
+    model = load_model(os.getenv("MODEL_NAME"))
+    start = time.time()
+    for _ in range(100):
+        model(torch.rand(1, 1, 48, 48))
+    end = time.time()
+    assert end - start < 1
+
+
+
+if __name__ == '__main__':
+    test_model_speed()
+
+
+
